e9fdaf68c090004659504455cc2cc42b3a996477
galdoben <galdobenedetta@gmail.com>
2024-11-26 09:43:10 +0100

    fix: main

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-const priceKm = 0.21;
-const discountUnder18 = 20;
-const discountOver65 = 40;
 
-let priceTicket = priceKm * kmPasseggero
+
+/*let priceTicket = priceKm * kmPasseggero
 const priceUnder18 = priceTicket * (1 - discountUnder18 / 100);
 const priceOver65 = priceTicket * (1 - discountOver65 / 100);
 let finalPrice
@@ -19,9 +17,40 @@ else {
     console.log('Il prezzo totale del ticket è', priceTicket .toFixed(2));
 
 }
-console.log(finalPrice)
+console.log(finalPrice)*/
+
+const priceKm = 0.21;
+const discountUnder18 = 20;
+const discountOver65 = 40;
+
+const name = document.getElementById('name-input');
+const km = document.getElementById('km-input');
+const menuAge = document.getElementById('age-input');
+const form = document.getElementById('info');
+const btn = document.getElementById('calc');
+const span = document.getElementById('price');
+console.log(km.value)
 
 
-const name = document.querySelector(input-group-a);
-const km = document.querySelector(input-group-b);
-const menuAge = document.querySelector(form-select);
+btn.addEventListener('click',()=>{
+let priceTicket = priceKm * km.value
+const priceUnder18 = priceTicket * (1 - discountUnder18 / 100);
+const priceOver65 = priceTicket * (1 - discountOver65 / 100);
+let finalPrice
+
+
+console.log(menuAge.value)
+if (menuAge.value === '1'){
+    finalPrice = priceUnder18
+    span.innerText = finalPrice.toFixed(2);
+}
+else if (menuAge.value === '3'){
+    finalPrice = priceOver65
+    console.log('Il prezzo finale è pari a', finalPrice .toFixed(2));
+}
+else {
+    console.log('Il prezzo totale del ticket è', priceTicket .toFixed(2));
+
+}
+ console.log(km.value)
+})
